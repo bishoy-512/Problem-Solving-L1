d17ab977d8cee7d57ff54468660e9591a6c4232d
Bishoy Amgad <bishoy.amgad71@gmail.com>
2026-02-25 22:35:40 +0200

    adding g ,t

--- DFS/T.cpp
+++ DFS/T.cpp
@@ -28,9 +28,15 @@ using namespace std;
 
 vector<bool>vis;
 vector<vector<int>>adj;
-
+int ans = LONG_LONG_MIN;
+int temp = 0;
 void dfs(int start){
     vis[start] = 1;
+    temp++;
+    if(adj[start].size() == 1){
+        ans = max(ans , temp);
+        temp = 0;
+    }
     for(auto i:adj[start]){
         if(!vis[i]){
             dfs(i);
@@ -40,42 +46,25 @@ void dfs(int start){
 
 void sol() {
     // Get Nodes , Edges
-    int n , m;cin>>n>>m;
+    int n , m;cin>>n;
     // Initialize All Needed Vectors using 1 indexed
     adj.resize(n+1,vector<int>());
     vis.resize(n+1,0);
     // Get Edges (m)
     unordered_set<int>city;
-    for(int i = 1;i <= m;i++){
+    for(int i = 1;i <= n-1;i++){
         int u , v;cin >> u >> v;
-        city.insert(u);
-        city.insert(v);
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-    int x = -1;
-    for(int i = 1;i<=n;i++){
-        if(!city.count(i)){
-            x = i;
-            break;
-        }
-    }
-    if(x == -1){
-        cout << n << endl;
-        for(int i = 2;i<=n;i++)
-            cout << 1 << " " << i << endl;
-        }
-        else{
-            cout << n - 1 << endl;
-            for(int i = 1;i<=n;i++){
-                if(x == i)
-                    continue;
-                cout << x << " " << i << endl;
-            }
-        }
     for(int i = 1; i <= n;i++){
         if(!vis[i]){
             dfs(i);
+            ans = max(ans , temp);
+            temp = 0;
         }
     }
+    cout << ans;
 }
 
 signed main() {
